cad7da11960248f93dcbf0f9c8f394a25736172b
Stark <serj.elessar@gmail.com>
2022-10-19 16:30:34 +0300

    init: start

---
+++ src/js/modules/index.js
@@ -0,0 +1,27 @@
+export function isWebp() {
+  // Проверка поддержки webp
+  function testWebp(callback) {
+    let webP = new Image()
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2)
+    }
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+  }
+  // Добавление класса _webp или _no-webp для HTML 
+  testWebp(function (support) {
+    let className = support === true ? 'webp' : 'no-webp'
+    document.documentElement.classList.add(className)
+  })
+}
+
+export function setburgerMenu() {
+  const burgerBtn = document.querySelector('.icon-menu')
+  const header = document.querySelector('.header')
+  const menu = document.querySelector('.menu')
+
+  burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('menu-open')
+    menu.classList.toggle('menu-open')
+    header.classList.toggle('menu-open')
+  })
+}
